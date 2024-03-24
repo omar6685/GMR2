f1f655ac2ecb199af818921164b5b7849ab42a71
omar6685 <omr6685@hotmail.com>
2024-03-24 16:54:06 +0300

    add image  to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   has_many :features, dependent: :destroy, inverse_of: :product
   has_many :highlights, dependent: :destroy, inverse_of: :product
   accepts_nested_attributes_for :features, reject_if: :all_blank, allow_destroy: true
+  has_many_attached :images
 end
