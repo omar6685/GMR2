a5203960f2c23651deea19bfbf786d7e084e3449
omar6685 <omr6685@hotmail.com>
2024-03-27 18:04:51 +0300

    edit product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
+  skip_before_action :authenticate_user!, only: [:show]
 
   # GET /products or /products.json
   def index
