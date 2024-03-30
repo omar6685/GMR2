8b00d7f7849cdcca9c509023123917ad82e45b33
omar6685 <omr6685@hotmail.com>
2024-03-30 14:58:55 +0300

    add ar

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Gmrr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.available_locales = [:en, :ar]
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,36 @@
 Rails.application.routes.draw do
-  resources :organizations
-  resources :information
-  resources :histories
-  resources :employees
-  resources :highlights
-  resources :features
-  resources :products
-  resources :socials
-  resources :logos
-  resources :legal_notices
-  resources :terms
-  resources :privacies
-  resources :contacts
-  resources :abouts
-  resources :home_sites
-  resources :home_boxes
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define routes within the default locale scope
+  scope "(:locale)", locale: /en|ar/ do
+    resources :organizations
+    resources :information
+    resources :histories
+    resources :employees
+    resources :highlights
+    resources :features
+    resources :products
+    resources :socials
+    resources :logos
+    resources :legal_notices
+    resources :terms
+    resources :privacies
+    resources :contacts
+    resources :abouts
+    resources :home_sites
+    resources :home_boxes
+    devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "static_public#home"
-  get "about_us", to: "static_public#about"
-  get "contact_us", to: "static_public#contact"
-  get "privacy_static", to: "static_public#privacy"
-  get "terms_and_conditions", to: "static_public#terms"
-  get "legal_notice_static", to: "static_public#legal_notice"
-  get "dashboard", to: "static_public#dashboard"
-  get "general_pages", to: "static_public#general_pages"
-  resources :home_videos , only: [:new, :create, :show]
-  resources :home_tests
+    # Define your application routes here
+
+    # Defines the root path route ("/")
+    root "static_public#home"
+    get "about_us", to: "static_public#about"
+    get "contact_us", to: "static_public#contact"
+    get "privacy_static", to: "static_public#privacy"
+    get "terms_and_conditions", to: "static_public#terms"
+    get "legal_notice_static", to: "static_public#legal_notice"
+    get "dashboard", to: "static_public#dashboard"
+    get "general_pages", to: "static_public#general_pages"
+    resources :home_videos, only: [:new, :create, :show]
+    resources :home_tests
+  end
 end
