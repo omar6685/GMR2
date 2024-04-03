888dca136cd9f7abca4816a05dcb44f63644589f
omar6685 <omr6685@hotmail.com>
2024-04-03 06:39:46 +0300

    edit sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -25,16 +25,26 @@ SitemapGenerator::Sitemap.create do
   #     add article_path(article), :lastmod => article.updated_at
   #   end
 
-  add root_path, :priority => 1, :changefreq => 'daily', :lastmod => Time.now
-  add about_us_path, :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
-  add contact_us_path, :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
-  add legal_notice_static_path, :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
-  add privacy_static_path, :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
-  add terms_and_conditions_path, :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
-  add dashboard_path, :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
-  add products_path, :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
-  Product.find_each do |product|
-    add product_path(locale: :en, id: product.id), :lastmod => product.updated_at
+  add root_path(locale: :en), :priority => 1, :changefreq => 'daily', :lastmod => Time.now
+  add root_path(locale: :ar), :priority => 1, :changefreq => 'daily', :lastmod => Time.now
+  add about_us_path(locale: :en), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add about_us_path(locale: :ar), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add contact_us_path(locale: :en), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add contact_us_path(locale: :ar), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add legal_notice_static_path(locale: :en), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add legal_notice_static_path(locale: :ar), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add privacy_static_path(locale: :en), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add privacy_static_path(locale: :ar), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add terms_and_conditions_path(locale: :en), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add terms_and_conditions_path(locale: :ar), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add dashboard_path(locale: :en), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add dashboard_path(locale: :ar), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add products_path(locale: :en), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  add products_path(locale: :ar), :priority => 0.7, :changefreq => 'weekly', :lastmod => Time.now
+  I18n.available_locales.each do |locale|
+    Product.find_each do |product|
+      add product_path(locale: locale, id: product.id), lastmod: product.updated_at, changefreq: 'weekly', priority: 0.8
+    end
   end
   
 
